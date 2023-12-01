184c4e47df6ade0f155233fb2943867d4ea9d1ef
OmgCopito95 <july1995bolt@gmail.com>
2023-12-01 18:48:11 -0300

    enunciados segunda fecha 2023

---
+++ Parciales/Parciales Segundo Semestre/SegundaFecha.pas
@@ -0,0 +1,30 @@
+// TEMA 1
+// "Un salón de eventos necesita un programa para administrar la información de las reservas de diciembre de 2023. 
+// Se dispone de una estructura con la información de las reservas. De cada reserva se conoce: número de reserva, 
+// DNI del cliente, día del evento (1..31), hora de inicio, hora de fin y categoría de servicio (1..4). 
+// Además, se dispone de una tabla con el precio por hora de reserva de acuerdo a cada categoría de servicio.
+
+// Se pide:
+
+// A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse 
+// ordenada por número de reserva.
+// B) Informar los dos días del mes con mayor cantidad de reservas de clientes con DNI par.
+// C) Informar el porcentaje de reservas de eventos que inicien antes de las 12 hs y se produzcan en la primera quincena.
+
+
+// -----------------------------------------------------------------------------------------------
+
+
+// TEMA 2
+// "Un salón de eventos necesita un programa para administrar la información de las reservas de diciembre de 2023. 
+// Se dispone de una estructura con la información de las reservas. De cada reserva se conoce: número de reserva, 
+// DNI del cliente, día del evento (1..31), hora de inicio, hora de fin y categoría de servicio (1..4). 
+// Además, se dispone de una tabla con el precio por hora de reserva de acuerdo a cada categoría de servicio.
+
+// Se pide:
+
+// A) Generar una nueva estructura con número y precio total de cada reserva. Esta estructura debe generarse ordenada 
+// por el precio total de cada reserva.
+// B) Informar los dos días del mes con menor cantidad de reservas de clientes con DNI impar.
+// C) Informar el porcentaje de reservas de eventos que inicien después de las 12 hs y se produzcan en la segunda quincena.
+
