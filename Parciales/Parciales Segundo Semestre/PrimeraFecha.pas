325e8605afd3ace2d09b6431718ead2d87734892
OmgCopito95 <july1995bolt@gmail.com>
2023-12-01 18:48:02 -0300

    Enunciado primera fecha 2023 segundo semestre

---
+++ Parciales/Parciales Segundo Semestre/PrimeraFecha.pas
@@ -0,0 +1,29 @@
+// TEMA 1
+// "Un restaurante necesita un programa para administrar la información de los platos que ofrece. 
+// Se dispone de una estructura con la información de los platos. 
+// De cada plato se conoce: nombre, precio, categoría (1: Entrada, 2: Principal, 3: Postre, 4: Minuta), 
+// cantidad de ingredientes y nombre de cada uno de los ingredientes (a lo sumo 10).
+
+// Se pide:
+
+// A) Generar una nueva estructura con nombre y precio de cada plato que posea ""perejil"" entre sus ingredientes.
+// B) Informar las dos categorías con mayor cantidad de platos ofrecidos.
+// C) Informar el precio promedio de los platos con más de 5 ingredientes.
+
+
+// ----------------------------------------------------------------------------------------
+
+
+// TEMA 2
+// "Una juguetería necesita un programa para administrar la información de los productos que comercializa. 
+// De cada producto se lee: código, descripción, categoría (1: Primera infancia, 2: Didácticos, 3: De mesa, 4: Otros), 
+// marca y precio. La lectura finaliza cuando se lee el código -1, que no debe procesarse. La información se lee ordenada por marca.
+
+// Se pide:
+
+// A) Generar una estructura que contenga, para cada marca, la cantidad de productos comercializados.
+// B) Informar las dos categorías con menor cantidad de productos comercializados.
+// C) Informar el precio promedio de los productos con código múltiplo de 3.
+
+
+
